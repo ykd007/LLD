54303bec4aff6b6bda1f2ad9287517a1f8a02dd7
Yashd <Yashd@Dhawan>
2024-07-18 20:26:38 +0530

    Log4j2 setup.

--- src/main/java/lld/chess/controller/GameController.java
+++ src/main/java/lld/chess/controller/GameController.java
@@ -1,5 +1,7 @@
 package lld.chess.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +14,15 @@ import lld.chess.service.User;
 @RequestMapping("game-controller")
 public class GameController {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(GameController.class);
+	
 	@Autowired
 	User user;
 
 	@GetMapping("test")
 	private ResponseEntity<String> test() {
+		
+		LOG.debug("In game-controller");
 		return user.test();
 	}
 }
--- src/main/java/lld/chess/service/Impl/UserImpl.java
+++ src/main/java/lld/chess/service/Impl/UserImpl.java
@@ -1,5 +1,7 @@
-package lld.chess.serviceImpl;
+package lld.chess.service.Impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,8 +11,12 @@ import lld.chess.service.User;
 @Service
 public class UserImpl implements User {
 
+	private static final Logger LOG = LoggerFactory.getLogger(UserImpl.class);
+	
 	@Override
 	public ResponseEntity<String> test() {
+		
+		LOG.debug("In service implementation");
 		return new ResponseEntity<>("Acknowledged", HttpStatus.OK);
 	}
 
